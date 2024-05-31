cc5d75e94b2a09f1300ce235dcc66539b6092dc0
Moyon Enzo <moyon_e@etna-alternance.net>
2024-05-31 10:11:37 +0200

    add: message handling

--- src/directives/index.svelte
+++ src/directives/index.svelte
@@ -1,4 +1,83 @@
 <script lang="ts">
+    import { get } from "svelte/store";
+    import identity from "../identity";
+
+    import Paperclip from "lucide-svelte/icons/paperclip";
+    import Mic from "lucide-svelte/icons/mic";
+    import CornerDownLeft from "lucide-svelte/icons/corner-down-left";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+    import { Textarea } from "$lib/components/ui/textarea/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+    import { ScrollArea } from "$lib/components/ui/scroll-area";
+
     import { controller as header } from "$lib/GlobalLayout.svelte";
+    import { socket } from "../main";
+    import Message from "$lib/Message.svelte";
     header.setTitle("Global Chat");
+
+    let messages: any = [];
+    socket.on('message', function(payload) {
+        messages = [...messages, payload]
+    })
+
+    let message: any = null;
+    async function sendMessage() {
+        socket.emit('message', message);
+        message = null;
+    }
+
+    async function keydown(e: any) {
+        if (e.code === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            document.getElementById('submitForm')?.click();
+        }
+    }
 </script>
+
+<div style="height: calc( 100vh - 250px )" id="scroll">
+    <ScrollArea class="w-full h-full p-4">
+        {#each messages as message}
+            <Message sender={message.identity} message={message.message} />
+        {/each}
+    </ScrollArea>
+</div>
+
+<form
+        class="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
+        on:submit|preventDefault={sendMessage}
+>
+    <Label for="message" class="sr-only">Message</Label>
+    <Textarea
+            id="message"
+            placeholder="Type your message here..."
+            class="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+            on:keydown={keydown}
+            bind:value={message}
+            required
+    />
+    <div class="flex items-center p-3 pt-0">
+        <Tooltip.Root>
+            <Tooltip.Trigger asChild let:builder>
+                <Button variant="ghost" size="icon" builders={[builder]} disabled>
+                    <Paperclip class="size-4" />
+                    <span class="sr-only">Attach file</span>
+                </Button>
+            </Tooltip.Trigger>
+            <Tooltip.Content side="top">Attach File</Tooltip.Content>
+        </Tooltip.Root>
+        <Tooltip.Root>
+            <Tooltip.Trigger asChild let:builder>
+                <Button variant="ghost" size="icon" builders={[builder]} disabled>
+                    <Mic class="size-4" />
+                    <span class="sr-only">Use Microphone</span>
+                </Button>
+            </Tooltip.Trigger>
+            <Tooltip.Content side="top">Use Microphone</Tooltip.Content>
+        </Tooltip.Root>
+        <Button type="submit" id="submitForm" size="sm" class="ml-auto gap-1.5">
+            Send Message
+            <CornerDownLeft class="size-3.5" />
+        </Button>
+    </div>
+</form>
---
+++ src/lib/Message.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { get } from "svelte/store";
+    import identity from "../identity";
+
+    export let sender: any;
+    export let message: string;
+
+    onMount(() => {
+        const scrolldiv = document.getElementById('scroll');
+        const last = scrolldiv?.children[0].children[1].children[0].lastElementChild;
+        last?.scrollIntoView({ behavior: 'smooth', block: 'end' })
+        last?.children[0].children[0].innerHTML.replace("\n", "<br />")
+    })
+</script>
+
+{#if $identity.id === sender.id}
+    <div class="flex flex-row justify-end mb-4">
+        <div>
+            <p class="break-all max-w-xl bg-primary text-white text-justify" style="padding: 10px; border-radius: 5px">
+                {@html message}
+            </p>
+        </div>
+    </div>
+{:else}
+    <div class="flex flex-row justify-start mb-4">
+        <div>
+            <p class="break-all max-w-xl bg-accent text-white text-justify" style="padding: 10px; border-radius: 5px">
+                {@html message}
+            </p>
+        </div>
+    </div>
+{/if}
